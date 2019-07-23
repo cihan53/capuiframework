ae68dbe18c01b9b264f0635e056f7dad0e8533a3
cihanozturk <cihanozturk53@gmail.com>
2019-07-23 16:33:59 +0300

    BaseStore Reset Düzenlemesi,
    Module düzenleme store hatası giderildi.
    
    (cherry picked from commit 0cb736c)

---
+++ dist/Lib/CAP/Form/myForm.js
@@ -0,0 +1,32 @@
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+function _possibleConstructorReturn(self, call) { if (call && (typeof call === "object" || typeof call === "function")) { return call; } return _assertThisInitialized(self); }
+
+function _assertThisInitialized(self) { if (self === void 0) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return self; }
+
+function _getPrototypeOf(o) { _getPrototypeOf = Object.setPrototypeOf ? Object.getPrototypeOf : function _getPrototypeOf(o) { return o.__proto__ || Object.getPrototypeOf(o); }; return _getPrototypeOf(o); }
+
+function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function"); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, writable: true, configurable: true } }); if (superClass) _setPrototypeOf(subClass, superClass); }
+
+function _setPrototypeOf(o, p) { _setPrototypeOf = Object.setPrototypeOf || function _setPrototypeOf(o, p) { o.__proto__ = p; return o; }; return _setPrototypeOf(o, p); }
+
+/**
+ * TODO burası hazılanacak
+ */
+import validatorjs from 'validatorjs';
+
+let myForm =
+/*#__PURE__*/
+function (_MobxReactForm) {
+  _inherits(myForm, _MobxReactForm);
+
+  function myForm() {
+    _classCallCheck(this, myForm);
+
+    return _possibleConstructorReturn(this, _getPrototypeOf(myForm).apply(this, arguments));
+  }
+
+  return myForm;
+}(MobxReactForm);
+
+export { myForm as default };
