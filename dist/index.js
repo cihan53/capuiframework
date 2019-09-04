44db376fcc8657d77a23e17574beba373e1c4292
cihanozturk <cihanozturk53@gmail.com>
2019-09-04 08:42:54 +0300

    Framework 1.1

--- dist/index.js
+++ dist/index.js
@@ -21,28 +21,83 @@ const DataProxy = CAPFrameWork.Data.Proxy;
 const BaseStore = CAPFrameWork.BaseStore;
 const StoreManager = CAPFrameWork.Stores;
 const BreadCrumb = CAPFrameWork.BreadCrumb;
+const ClassNames = CAPFrameWork.ClassNames;
+const BaseController = CAPFrameWork.BaseController;
+/**
+ *
+ * @type {Mask}
+ */
+
+const Mask = CAPFrameWork.Mask;
+const Spinner = CAPFrameWork.Spinner;
+/**
+ *
+ * @type {React.ComponentType<{} & React.ClassAttributes<unknown>>}
+ */
+
+const Grid = CAPFrameWork.Grid;
+/**
+ *
+ * @type {{postMultiPart, postJson, post, get, del, deadline, postForm, timeout, put}}
+ */
+
+const Request = CAPFrameWork.Request;
+/**
+ *
+ */
+
 const MessageBox = CAPFrameWork.MessageBox;
 const ConfirmBox = CAPFrameWork.Message.Confirm;
-const BaseController = CAPFrameWork.BaseController;
-const Spinner = CAPFrameWork.Spinner;
-const Grid = CAPFrameWork.Grid;
-const Request = CAPFrameWork.Request;
-const ClassNames = CAPFrameWork.ClassNames;
+/**
+ *
+ * @type {Form|{Field, JsonInput, form, JsonSchemaEditor}}
+ */
+
 const Form = CAPFrameWork.Form;
 const FormPanel = CAPFrameWork.FormPanel;
+const Button = CAPFrameWork.Form.Field.Button;
+/**
+ * Action View
+ * @type {View}
+ */
+
 const View = CAPFrameWork.View.View;
+/**
+ *
+ * @type {Panel}
+ */
+
 const Panel = CAPFrameWork.Panel;
+const AlertPanel = CAPFrameWork.AlertPanel;
 const Loadable = CAPFrameWork.Loadable;
+/**
+ *
+ * @type {{oneOfType, symbol, bool, string, shape, arrayOf, any, instanceOf, number, node, objectOf, oneOf, func, array, exact, elementType, object, element}|{oneOfType, symbol, checkPropTypes, bool, string, shape, arrayOf, any, instanceOf, number, node, objectOf, oneOf, func, array, exact, resetWarningCache, elementType, object, element}}
+ */
+
 const PropTypes = CAPFrameWork.PropTypes;
+/**
+ *
+ * @type {moment | ((inp?: moment.MomentInput, format?: moment.MomentFormatSpecification, language?: string, strict?: boolean) => moment.Moment) | ((inp?: moment.MomentInput, format?: moment.MomentFormatSpecification, strict?: boolean) => moment.Moment)}
+ */
+
 const Date = CAPFrameWork.Date;
 const ShortId = CAPFrameWork.ShortId;
+/**
+ *
+ * @type {{[p: string]: *}}
+ */
+
 const Utils = CAPFrameWork.Utils;
-const Button = CAPFrameWork.Form.Field.Button;
-const AlertPanel = CAPFrameWork.AlertPanel;
+/**
+ *
+ * @type {Raise}
+ */
+
 const Raise = CAPFrameWork.Raise;
 const Log = CAPFrameWork.Log;
 const Logger = CAPFrameWork.Logger;
 const Debug = CAPFrameWork.Debug;
 export default { ...CAPFrameWork
 };
-export { CAP, DataProxy, BreadCrumb, MessageBox, ConfirmBox, BaseController, Date, Spinner, ShortId, Grid, Request, StoreManager, BaseStore, ClassNames, FormPanel, Panel, Form, AlertPanel, Button, Loadable, PropTypes, Xtypes, View, Utils, Raise, Log, Debug, Logger };
+export { CAP, DataProxy, BreadCrumb, MessageBox, ConfirmBox, BaseController, Date, Mask, Spinner, ShortId, Grid, Request, StoreManager, BaseStore, ClassNames, FormPanel, Panel, Form, AlertPanel, Button, Loadable, PropTypes, Xtypes, View, Utils, Raise, Log, Debug, Logger };
